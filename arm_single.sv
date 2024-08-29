e837f8aa9afdccde6783e752144ad7a5cb80a8cb
urigabriel <urigabriel1404@gmail.com>
2024-08-28 23:09:19 -0300

    Implementação MovF

--- arm_single.sv
+++ arm_single.sv
@@ -95,21 +95,7 @@ module testbench();
     begin
       clk <= 1; # 5; clk <= 0; # 5;
     end
-
-  // check results
-  //always @(negedge clk)
-    //begin
-      //if(MemWrite) begin
-        //if(DataAdr === 100 & WriteData === 7) begin
-          //$display("Simulation succeeded");
-          //$stop;
-        //end else if (DataAdr !== 96) begin
-          //$display("Simulation failed");
-          //$stop;
-       //end
-      //end
-    //end
-//endmodule
+endmodule
 
 module top(input  logic        clk, reset, 
            output logic [31:0] WriteData, DataAdr, 
